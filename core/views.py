16ffa26efe5daa3ba33fc4706a5b2ced27b136f9
Luis Vilella <luivilella@gmail.com>
2018-03-01 09:44:01 -0300

    chore(tweets-by-countries): create a new view
    Create a new view to return tweets-by-countries

--- core/views.py
+++ core/views.py
@@ -1,7 +1,20 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from django.views import View
+from .tools.twitter.get_tweets_by_country import get_tweets_by_country
+from .tools.cache import cache
 
 
 class IndexView(View):
     def get(self, request):
         return render(request, 'core/index-page.html')
+
+
+class TweetsByCountriesView(View):
+
+    @cache.cache('TweetsByCountriesView.get_results', expire=120)
+    def get_results(self):
+        return dict(data=get_tweets_by_country())
+
+    def get(self, request):
+        return JsonResponse(self.get_results())
